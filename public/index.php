2bdc04b0667411d197ec744238f162a6f53d61b6
Dimitar Dimitrov <daghostman.dimitrov@gmail.com>
2016-01-25 19:51:20 +0000

    Removed global error-config (legacy from experimenting) in index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 1);
 // Delegate static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server'
     && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
